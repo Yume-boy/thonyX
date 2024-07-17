2e8cd12444490a548060a9e08d4987a28e3da651
marvellous <comlaf2001@gmail.com>
2024-07-17 22:59:46 +0100

    lol

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -9,15 +9,15 @@ const ContactForm = () => {
             <Form action="https://formspree.io/f/mzzpzonn" method="POST">
                 <Form.Group controlId="formName">
                 <Form.Label>Name</Form.Label>
-                <Form.Control type="text" placeholder="Enter your name" />
+                <Form.Control type="text" placeholder="Enter your name" name='Name' />
                 </Form.Group>
                 <Form.Group controlId="formEmail">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter your email" />
+                <Form.Control type="email" placeholder="Enter your email" name='Email' />
                 </Form.Group>
                 <Form.Group controlId="formMessage">
                 <Form.Label>Message</Form.Label>
-                <Form.Control as="textarea" rows={3} />
+                <Form.Control as="textarea" rows={3} name='Message'/>
                 </Form.Group>
                 <Button variant="primary" type="submit" className="mt-3">
                 Submit
