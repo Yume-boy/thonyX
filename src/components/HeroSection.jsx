f2e628e9fcbd852e43c83592486906fa521aa457
marvellous <comlaf2001@gmail.com>
2024-07-18 15:02:40 +0100

    lol

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -5,7 +5,7 @@ const HeroSection = () => {
   return (
     <div className="hero-section">
       <div className="overlay">
-        <img src="../public/heroImage.jpg" alt="Individual" className="center-image" />
+        <img src="/public/heroImage.jpg" alt="Individual" className="center-image" />
         
         <h2>Anthony</h2>
       </div>
